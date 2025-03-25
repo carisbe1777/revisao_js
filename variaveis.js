f05d5b15e285215b6c9cf0c216bc5c9441d2a237
carisbe1777 <carisbe066@gmail.com>
2025-03-25 14:21:49 +0000

    feito

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+var nome = "carlos" //variavel
+let idade = 17 //variavel
+const signo = "aries" //constante
+
+/*
+a variavel var pode mudar o valor armazenado em qualquer momento
+a variavel let nao pode mudar o valor
+a variavel const nao pode mudar o valor
+*/
